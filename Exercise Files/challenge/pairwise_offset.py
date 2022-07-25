5d674a69862a7dcd27e351e99601138bd218d272
phoenitunes <70701513+phoenitunes@users.noreply.github.com>
2022-07-25 15:53:11 +0100

    exercise 3

--- Exercise Files/challenge/pairwise_offset.py
+++ Exercise Files/challenge/pairwise_offset.py
@@ -1,42 +1,40 @@
 from textwrap import fill
-
-
-def pairwise_offset(sequence, fillvalue, offset):
+import sys
+def small_offset(sequence, fillvalue, offset):
     tups=[]
-    
-    if offset==None or type(offset)==str:
-            print("Please enter a valid offset value")
-    elif 0<=offset<len(sequence): 
+
+    for i in range(0,(len(sequence)+ (offset))):
+        if i<offset:
+          tups.append((sequence[i],fillvalue))
+        elif i>=len(sequence):
+            tups.append((fillvalue,sequence[i-offset]))
+        else:
+             tups.append((sequence[i],sequence[i-offset]))  
+             
+    return tups         
+         
+         
+def big_offset(sequence, fillvalue, offset):
+    tups=[]
+
+    for i in range (0,len(sequence)): 
+        tups.append((sequence[i], fillvalue))
+    for i in range (len(sequence), offset): 
+        tups.append((fillvalue, fillvalue))    
+    for i in range (offset, len(sequence)+offset): 
+        tups.append((fillvalue, sequence[i-offset]))  
+    return tups         
         
-        for i in range(0,(len(sequence)+ (offset))):
-            if i<offset:
-                tups.append((sequence[i],fillvalue))
-                print(tups)
-            elif i>=len(sequence):
-                print("here",i-offset)
-                tups.append((fillvalue,sequence[i-offset]))
-                print(sequence[i-offset])
 
-            else:
-                print(i)
-                print("here I am ",sequence[i-offset])
+       
+       
+        
+    
+def pairwise_offset(sequence, fillvalue="*", offset=0):
+    if 0<=offset<len(sequence): 
+       tups= small_offset(sequence, fillvalue, offset)                             
+    elif offset>len(sequence): 
+       tups=  big_offset(sequence, fillvalue, offset)
+    return print(tups)   
 
-                tups.append((sequence[i],sequence[i-offset]))       
-
-                print(tups)
-                    
-    elif offset>len(sequence):
-        for i in range (0,len(sequence)): 
-            tups.append((sequence[i],fillvalue))
-        for i in range (len(sequence),offset): 
-            tups.append((fillvalue,fillvalue))    
-        for i in range (offset,len(sequence)+offset): 
-            tups.append((fillvalue,sequence[i-offset]))  
-               
-            
-
-
-
-    return print(tups)    
-
-pairwise_offset(['a','b','c'],"*",10)
+pairwise_offset(['a','b','c'],"*",5)
