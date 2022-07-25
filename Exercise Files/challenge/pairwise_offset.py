009db2624c1811b044c4f10a4d8beabaaba63cb8
phoenitunes <70701513+phoenitunes@users.noreply.github.com>
2022-07-25 17:03:31 +0100

    exercise 3 corrected tuple list

--- Exercise Files/challenge/pairwise_offset.py
+++ Exercise Files/challenge/pairwise_offset.py
@@ -2,14 +2,15 @@ from textwrap import fill
 import sys
 def small_offset(sequence, fillvalue, offset):
     tups=[]
+    # expected = [('a', 'a'), ('b', 'b'), ('c', 'c'), ('d', 'd'), ('e', 'e')]
 
     for i in range(0,(len(sequence)+ (offset))):
         if i<offset:
-          tups.append((sequence[i],fillvalue))
+            tups.append(tuple([sequence[i], fillvalue]))
         elif i>=len(sequence):
-            tups.append((fillvalue,sequence[i-offset]))
+            tups.append(tuple([fillvalue, sequence[i-offset]]))
         else:
-             tups.append((sequence[i],sequence[i-offset]))  
+            tups.append(tuple([sequence[i], sequence[i-offset]]))
              
     return tups         
          
@@ -18,23 +19,22 @@ def big_offset(sequence, fillvalue, offset):
     tups=[]
 
     for i in range (0,len(sequence)): 
-        tups.append((sequence[i], fillvalue))
+        tups.append(tuple([sequence[i], fillvalue]))
     for i in range (len(sequence), offset): 
-        tups.append((fillvalue, fillvalue))    
+        tups.append(tuple([fillvalue,fillvalue])  )  
     for i in range (offset, len(sequence)+offset): 
-        tups.append((fillvalue, sequence[i-offset]))  
-    return tups         
+        tups.append(tuple([fillvalue,sequence[i-offset]]) ) 
+    return tups      
         
 
        
        
         
     
-def pairwise_offset(sequence, fillvalue="*", offset=0):
+def pairwise_offset(sequence, fillvalue='*', offset=0):
     if 0<=offset<len(sequence): 
        tups= small_offset(sequence, fillvalue, offset)                             
     elif offset>len(sequence): 
        tups=  big_offset(sequence, fillvalue, offset)
-    return print(tups)   
+    return tups
 
-pairwise_offset(['a','b','c'],"*",5)
--- Exercise Files/challenge/pairwise_offset_solution.py
+++ Exercise Files/challenge/pairwise_offset_solution.py
@@ -3,3 +3,7 @@ from itertools import tee, zip_longest, chain
 def pairwise_offset(sequence, fillvalue='*', offset=0):
     it1, it2 = tee(sequence, 2)
     return zip_longest(it1, chain(fillvalue * offset, it2), fillvalue=fillvalue)
+
+k= pairwise_offset('abcde')
+
+print(list(k))
