28cb9ed94a17e2fc381476d6783db172f4475d5d
phoenitunes <70701513+phoenitunes@users.noreply.github.com>
2022-07-26 09:55:44 +0100

    exercise 5

--- Exercise Files/challenge/calculator.py
+++ Exercise Files/challenge/calculator.py
@@ -1,2 +1,16 @@
+from io import StringIO
+import sys
+
 class Calculator:
-    pass
+
+    def __init__(self,*errors):
+        self.errors=errors
+        
+    def __enter__(self):
+        return self
+
+    def __exit__(self,error_type,error,error_traceback_message):
+        self.error= error
+        return isinstance(error, self.errors)
+        
+
